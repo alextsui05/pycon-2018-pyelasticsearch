ab061f54276f702437bfc9d03a1bad1fd1ee3338
Alex Tsui <alextsui05@gmail.com>
2018-06-23 22:40:10 +0000

    Index all products

--- searchapp/index_products.py
+++ searchapp/index_products.py
@@ -17,7 +17,8 @@ def main():
         },
     )
 
-    index_product(es, all_products()[0])
+    for product in all_products():
+        index_product(es, product)
 
 
 def index_product(es, product: ProductData):
